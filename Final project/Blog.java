bfeec12602df1027f82b075a158c9c45025a29a5
Mahima Pattanayak <164752721+Mahima-Pattanayak@users.noreply.github.com>
2024-07-09 14:30:08 +0530

    Create Blog.java

---
+++ Final project/Blog.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Blog{
+    private List<User> users;
+    private List<Post> posts;
+
+    public Blog() {
+        this.users = new ArrayList<>();
+        this.posts = new ArrayList<>();
+    }
+
+    // Register a new user
+    public void registerUser(String username, String password) {
+        User user = new User(username, password);
+        users.add(user);
+    }
+
+    // Getters
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    // Create a new post
+    public void createPost(User user, String title, String content) {
+        user.createPost(title, content);
+    }
+
+    // Add comment to a post
+    public void addComment(User user, Post post, String content) {
+        user.addCommentToPost(post, content);
+    }
+}

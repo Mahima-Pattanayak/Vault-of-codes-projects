0a47d28d96beef6ab0dc7e7453c661904ec7828a
Mahima Pattanayak <164752721+Mahima-Pattanayak@users.noreply.github.com>
2024-07-09 14:28:00 +0530

    Update Post.java

--- Final project/Post.java
+++ Final project/Post.java
@@ -2,7 +2,7 @@ import java.util.Date;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Post {
+public class Post{
     private String title;
     private String content;
     private String author;

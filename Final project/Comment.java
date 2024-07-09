22013afbb0c25cf66130e6af0476a0926f156420
Mahima Pattanayak <164752721+Mahima-Pattanayak@users.noreply.github.com>
2024-07-09 14:27:40 +0530

    Create Comment.java

---
+++ Final project/Comment.java
@@ -0,0 +1,26 @@
+import java.util.Date;
+
+public class Comment{
+    private String content;
+    private String commenter;
+    private Date commentDate;
+
+    public Comment(String content, String commenter) {
+        this.content = content;
+        this.commenter = commenter;
+        this.commentDate = new Date(); // Current date and time
+    }
+
+    // Getters
+    public String getContent() {
+        return content;
+    }
+
+    public String getCommenter() {
+        return commenter;
+    }
+
+    public Date getCommentDate() {
+        return commentDate;
+    }
+}
